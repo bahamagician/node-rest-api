77ac729603568a4e035c6c103ea597f807ac41ec
SC Graphics <scgraphics@SCs-iMac.local>
2018-06-14 15:32:58 -0400

    Replace lodash merge with Object.assign

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,8 +1,7 @@
 import { makeExecutableSchema } from "graphql-tools";
 import { typeDef as Blog, resolvers as BlogResolvers } from "./blog.js";
-import { merge } from "lodash";
 
 export const schema = makeExecutableSchema({
   typeDefs: [Blog],
-  resolvers: merge(BlogResolvers)
+  resolvers: Object.assign(BlogResolvers)
 });
