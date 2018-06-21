0b5d2c1b00ad749a59fce96dfb221ad5e6d6cd35
SC Graphics <scgraphics@SCs-iMac.local>
2018-06-21 16:38:04 -0400

    Fix auth error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = function(req, res, next) {
-  const token = req.header("Authorization").replace("Bearer ", "");
-  if (!token) next();
+module.exports = async function(req, res, next) {
+  let token = await req.header("Authorization");
+  if (!token || !token.includes("Bearer ")) {
+    return next();
+  }
+  token = token.replace("Bearer ", "");
   try {
     const decoded = jwt.verify(token, process.env.SECRET);
     req.user = decoded;
